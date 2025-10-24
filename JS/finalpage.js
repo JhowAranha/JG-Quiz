08ef71f35193e91867668bde250a2fe301314af7
JhowAranha <jhonatan.aranha@hotmail.com>
2025-10-24 14:01:01 -0300

    Mudanças de tailwind feitas

--- JS/finalpage.js
+++ JS/finalpage.js
@@ -18,13 +18,13 @@ document.addEventListener("DOMContentLoaded", () => {
     console.log(localStorage.getItem("time"))
 
     if (score <= 5) {
-        congratsTitleDOM.innerText = "Você está começando sua jornada!"
-        congratsSubtitleDOM.innerText = "Não desanime, cada passo é uma oportunidade para aprender mais sobre a Palavra de Deus. Continue estudando e logo você vai se surpreender com seu progresso."
-        verseDOM.innerText = '📖"Esforça-te, e tem bom ânimo; não temas, nem te espantes." (Josué 1:9)'
+        congratsTitleDOM.innerText = "Você está começando sua jornada!";
+        congratsSubtitleDOM.innerText = "Não desanime, cada passo é uma oportunidade para aprender mais sobre a Palavra de Deus. Continue estudando e logo você vai se surpreender com seu progresso.";
+        verseDOM.innerText = '📖"Esforça-te, e tem bom ânimo; não temas, nem te espantes." (Josué 1:9)';
     } else if (score > 5 && score <= 10) {
-        textDOM.innerHTML = `<h1>Excelente!</h1>
-        Seu conhecimento bíblico é impressionante! Continue firme em sua caminhada, compartilhando e vivendo a Palavra no dia a dia.
-        <p style="font-style: italic; font-weight: bold">📖"Bem-aventurados os que ouvem a Palavra de Deus e a guardam." (Lucas 11:28)`
+        congratsTitleDOM.innerText = "Excelente!";
+        congratsSubtitleDOM.innerText = "Seu conhecimento bíblico é impressionante! Continue firme em sua caminhada, compartilhando e vivendo a Palavra no dia a dia.";
+        verseDOM.innerText = '📖"Bem-aventurados os que ouvem a Palavra de Deus e a guardam." (Lucas 11:28)';
     }
 
 })
